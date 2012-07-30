916ff951fc9b3c673a8ea5bef4b5e1f789ff74a8
Pekka Palmu <pekka.palmu@gmail.com>
2012-07-30 15:28:19 +0300

    Add iOS proto

---
+++ ios/UnityAdsExample/UnityAdsAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  UnityAdsAppDelegate.h
+//  UnityAdsExample
+//
+//  Created by bluesun on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class UnityAdsViewController;
+
+@interface UnityAdsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UnityAdsViewController *viewController;
+
+@end
---
+++ ios/UnityAdsExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UnityAdsExample
+//
+//  Created by bluesun on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UnityAdsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UnityAdsAppDelegate class]));
+    }
+}
