3a4ac34fc489e9a4359b2a3ee80cfa0112f0d324
Johan Halin <johan.halin@karppinen.fi>
2012-09-07 14:52:58 +0300

    Create a directory for our cache.

--- ios/UnityAds/UnityAdsCache.m
+++ ios/UnityAds/UnityAdsCache.m
@@ -33,7 +33,7 @@ NSString const * kUnityAdsCacheFilePathKey = @"kUnityAdsCacheFilePathKey";
 	if (paths == nil || [paths count] == 0)
 		return nil;
 	
-	return [[paths objectAtIndex:0] stringByAppendingString:@"/"];
+	return [[paths objectAtIndex:0] stringByAppendingString:@"/applifier/"];
 }
 
 - (void)_queueCampaignDownload:(UnityAdsCampaign *)campaign;
@@ -117,6 +117,13 @@ NSString const * kUnityAdsCacheFilePathKey = @"kUnityAdsCacheFilePathKey";
 
 - (void)cacheCampaigns:(NSArray *)campaigns
 {
+	NSError *error = nil;
+	if ( ! [[NSFileManager defaultManager] createDirectoryAtPath:[self _cachePath] withIntermediateDirectories:YES attributes:nil error:&error])
+	{
+		NSLog(@"Couldn't create cache path. Error: %@", error);
+		return;
+	}
+	
 	// TODO: check queue for existing downloads that should be cancelled
 	
 	for (UnityAdsCampaign *campaign in campaigns)
