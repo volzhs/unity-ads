11ce0f1c18ecd9699d15c0912c1318327007ae34
Johan Halin <johan.halin@karppinen.fi>
2012-09-18 09:47:26 +0300

    Disabled vertical scroll indicator in the web view.

--- ios/UnityAds/UnityAdsiOS4.m
+++ ios/UnityAds/UnityAdsiOS4.m
@@ -298,7 +298,10 @@ typedef enum
 	}
 	
 	if (scrollView != nil)
+	{
 		scrollView.delegate = self;
+		scrollView.showsVerticalScrollIndicator = NO;
+	}
 	
 	[self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:kUnityAdsTestWebViewURL]]];
 	[self.adsWindow addSubview:self.webView];
