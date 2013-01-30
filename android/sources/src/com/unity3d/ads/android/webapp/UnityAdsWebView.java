25cae477b8be9fec4055cc8195f7a0b72587238a
Pekka Palmu <pekka.palmu@gmail.com>
2013-01-30 11:01:59 +0200

    Support touch events on android =< 2.3

--- android/sources/src/com/unity3d/ads/android/webapp/UnityAdsWebView.java
+++ android/sources/src/com/unity3d/ads/android/webapp/UnityAdsWebView.java
@@ -12,6 +12,7 @@ import com.unity3d.ads.android.properties.UnityAdsProperties;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Build;
 import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebChromeClient;
@@ -117,13 +118,15 @@ public class UnityAdsWebView extends WebView {
 		setupUnityAdsView();
 		loadUrl(_url);
 		
-		setOnLongClickListener(new OnLongClickListener() {
-			@Override
-			public boolean onLongClick(View v) {
-			    return true;
-			}
-		});
-		setLongClickable(false);
+		if (Build.VERSION.SDK_INT > 8) {
+			setOnLongClickListener(new OnLongClickListener() {
+				@Override
+				public boolean onLongClick(View v) {
+				    return true;
+				}
+			});
+			setLongClickable(false);
+		}
 	}
 	
 	private void setupUnityAdsView ()  {
@@ -141,7 +144,7 @@ public class UnityAdsWebView extends WebView {
 		
 		getSettings().setSupportZoom(false);
 		getSettings().setBuiltInZoomControls(false);
-		getSettings().setLightTouchEnabled(false);
+		//getSettings().setLightTouchEnabled(false);
 		getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
 		getSettings().setSupportMultipleWindows(false);
 		getSettings().setPluginsEnabled(false);
@@ -154,9 +157,7 @@ public class UnityAdsWebView extends WebView {
 		setFocusable(true);
 		setFocusableInTouchMode(true);
 		setInitialScale(0);
-		
 
-		
 		setBackgroundColor(Color.BLACK);
 		setBackgroundDrawable(null);
 		setBackgroundResource(0);
