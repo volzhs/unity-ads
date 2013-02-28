7183618ae44fa328d8bd590507c4a2fcd14f0a3a
Pekka Palmu <pekka.palmu@gmail.com>
2013-02-28 17:10:08 +0200

    Cleanup code

--- unity/unityads-project/Assets/Plugins/UnityAds/UnityAdsOpenButton.cs
+++ unity/unityads-project/Assets/Plugins/UnityAds/UnityAdsOpenButton.cs
@@ -5,14 +5,6 @@ using System.Collections.Generic;
 public class UnityAdsOpenButton : MonoBehaviour {
 	private bool _canOpen = false;
 	
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	}
-	
 	void OnGUI () {
 		if (UnityAds.getTestButtonVisibility()) {
 			string buttonText = "Waiting...";
@@ -27,22 +19,6 @@ public class UnityAdsOpenButton : MonoBehaviour {
 				if (_canOpen) {
 					UnityAdsExternal.Log("Open Unity Ads -button clicked");
 					UnityAds.show();
-				}	
-			}
-			
-			if (GUI.Button (new Rect (10,70,170,50), "Show rewardItemKeys")) {
-				if (_canOpen) {
-					List<string> keys = UnityAds.getRewardItemKeys();
-					foreach (string key in keys) {
-						UnityAdsExternal.Log("Reward key: " + key);
-					}
-				}	
-			}
-			
-			if (GUI.Button (new Rect (10,130,170,50), "Reward item details")) {
-				if (_canOpen) {
-					UnityAdsExternal.Log("Trying to fetch details with key: " + UnityAds.getCurrentRewardItemKey());
-					UnityAds.getRewardItemDetailsWithKey(UnityAds.getCurrentRewardItemKey());
 				}	
 			}
 		}
