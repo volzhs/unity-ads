bae52ed778ac838bf15f36effb374373aa90f40a
Matti Savolainen <matti@applifier.com>
2013-05-06 13:42:31 +0300

    fix simulator build

---
+++ ios/UnityAds/UnityAdsVideo/UnityAdsVideoOverlay.h
@@ -0,0 +1,13 @@
+//
+//  UnityAdsVideoOverlay.h
+//  UnityAds
+//
+//  Created by Matti Savolainen on 5/6/13.
+//  Copyright (c) 2013 Unity Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UnityAdsVideoOverlay : UIView
+
+@end
---
+++ ios/UnityAds/UnityAdsVideo/UnityAdsVideoOverlay.m
@@ -0,0 +1,23 @@
+//
+//  UnityAdsVideoOverlay.m
+//  UnityAds
+//
+//  Created by Matti Savolainen on 5/6/13.
+//  Copyright (c) 2013 Unity Technologies. All rights reserved.
+//
+
+#import "UnityAdsVideoOverlay.h"
+
+@implementation UnityAdsVideoOverlay
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+
+@end
