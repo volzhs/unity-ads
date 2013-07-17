d741362d5ec1281c6c130b85cc8c3ac83168e409
Ville Orkas <ville.orkas@applifier.com>
2013-07-17 16:27:49 +0300

    Bump iOS version to 109

--- ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
+++ ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
@@ -11,7 +11,7 @@
 #import "../UnityAds.h"
 #import "../UnityAdsDevice/UnityAdsDevice.h"
 
-NSString * const kUnityAdsVersion = @"108";
+NSString * const kUnityAdsVersion = @"109";
 
 @implementation UnityAdsProperties
 
