8de0ac4f095fe8a58e013606b9e4c67d1a617bf1
Ville Orkas <ville.orkas@applifier.com>
2013-10-09 11:26:23 +0300

    Remove unused public queryUrl

--- ios/UnityAds/UnityAdsData/UnityAdsAnalyticsUploader.m
+++ ios/UnityAds/UnityAdsData/UnityAdsAnalyticsUploader.m
@@ -119,22 +119,6 @@ static UnityAdsAnalyticsUploader *sharedUnityAdsInstanceAnalyticsUploader = nil;
   dispatch_release(self.analyticsQueue);
 }
 
-
-#pragma mark - Public
-
-- (void)queueUrl:(NSString *)url {
-  if (url != nil) {
-    UAAssert(![NSThread isMainThread]);
-    
-    NSArray *queryStringComponents = [url componentsSeparatedByString:@"?"];
-    NSString *urlPath = [queryStringComponents objectAtIndex:0];
-    NSString *queryString = [queryStringComponents objectAtIndex:1];
-    
-    [self _queueWithURLString:urlPath queryString:queryString httpMethod:@"GET" retries:[NSNumber numberWithInt:0]];
-  }
-}
-
-
 #pragma mark - Click track
 
 - (void)sendOpenAppStoreRequest:(UnityAdsCampaign *)campaign {
