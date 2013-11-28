09615b2a62f4ccb6ca88c5647021cab3436b005c
Ville Orkas <ville.orkas@applifier.com>
2013-11-28 14:15:11 +0200

    More options merge tests & tests for gamer sid

--- android/unittests/src/com/mycompany/test/test/UnityAdsRewardItemManagerTest.java
+++ android/unittests/src/com/mycompany/test/test/UnityAdsRewardItemManagerTest.java
@@ -109,27 +109,4 @@ public class UnityAdsRewardItemManagerTest extends ActivityInstrumentationTestCa
 		assertNull(itemManager.getCurrentItem());
 	}
 	
-	/*
-	 * - (void)testEmptyItems {
-  UnityAdsRewardItemManager * itemManager = [[UnityAdsRewardItemManager alloc] initWithItems:@{} defaultItem:validItem1];
-  STAssertTrue(itemManager == nil, @"Invalid item manager was created");
-}
-
-- (void)testEmptyDefaultItem {
-  UnityAdsRewardItemManager * itemManager = [[UnityAdsRewardItemManager alloc] initWithItems:@{@"testItemKey1": validItem1} defaultItem:nil];
-  STAssertTrue(itemManager == nil, @"Invalid item manager was created");
-}
-
-- (void)testMissingDefaultItem {
-  UnityAdsRewardItemManager * itemManager = [[UnityAdsRewardItemManager alloc] initWithItems:@{@"testItemKey1": validItem1} defaultItem:validItem2];
-  STAssertTrue(itemManager == nil, @"Invalid item manager was created");
-}
-
-- (void)testValidItems {
-  UnityAdsRewardItemManager * itemManager = [[UnityAdsRewardItemManager alloc] initWithItems:@{@"testItemKey1": validItem1, @"testItemKey2": validItem2} defaultItem:validItem2];
-  STAssertTrue(itemManager != nil, @"Valid item manager was not created");
-  STAssertTrue([itemManager itemCount] == 2, @"Invalid item count");
-}
-	 */
-	
 }
--- android/unittests/src/com/mycompany/test/test/UnityAdsZoneTest.java
+++ android/unittests/src/com/mycompany/test/test/UnityAdsZoneTest.java
@@ -10,6 +10,7 @@ import android.test.ActivityInstrumentationTestCase2;
 import com.mycompany.test.UnityAdsTestStartActivity;
 import com.unity3d.ads.android.zone.UnityAdsZone;
 
+@SuppressWarnings("serial")
 public class UnityAdsZoneTest extends ActivityInstrumentationTestCase2<UnityAdsTestStartActivity> {
 
 	private UnityAdsZone validZone;
@@ -19,7 +20,6 @@ public class UnityAdsZoneTest extends ActivityInstrumentationTestCase2<UnityAdsT
 	}
 	
 	@Override
-	@SuppressWarnings("serial")
 	public void setUp() throws Exception {
 		super.setUp();
 		JSONObject validZoneObject = new JSONObject(new HashMap<String, Object>(){{
@@ -34,7 +34,6 @@ public class UnityAdsZoneTest extends ActivityInstrumentationTestCase2<UnityAdsT
 		validZone = new UnityAdsZone(validZoneObject);
 	}
 	
-	@SuppressWarnings("serial")
 	public void testZoneValidOverrides() {
 		validZone.mergeOptions(new HashMap<String, Object>(){{
 			put("openAnimated", false);
@@ -42,12 +41,39 @@ public class UnityAdsZoneTest extends ActivityInstrumentationTestCase2<UnityAdsT
 		assertTrue(!validZone.openAnimated());
 	}
 	
-	@SuppressWarnings("serial")
 	public void testZoneInvalidOverrides() {
 		validZone.mergeOptions(new HashMap<String, Object>(){{
 			put("muteVideoSounds", true);
 		}});
 		assertTrue(!validZone.muteVideoSounds());
+	}
+	
+	public void testZoneInitialOptionsMerge() {
+		validZone.mergeOptions(new HashMap<String, Object>(){{
+			put("openAnimated", false);
+		}});
+		assertTrue(!validZone.openAnimated());
+		validZone.mergeOptions(new HashMap<String, Object>(){{
+			put("noOfferScreen", true);
+		}});
+		assertTrue(!validZone.muteVideoSounds());
+		assertTrue(validZone.noOfferScreen());
+	}
+	
+	public void testZoneSetSid() {
+		validZone.mergeOptions(new HashMap<String, Object>(){{
+			put("sid", "testSid");
+		}});
+		assertTrue(validZone.getGamerSid().equals("testSid"));
+	}
+	
+	public void testZoneRemoveSid() {
+		validZone.mergeOptions(new HashMap<String, Object>(){{
+			put("sid", "testSid");
+		}});
+		assertTrue(validZone.getGamerSid().equals("testSid"));
+		validZone.mergeOptions(null);
+		assertTrue(validZone.getGamerSid() == null);
 	}
 	
 }
