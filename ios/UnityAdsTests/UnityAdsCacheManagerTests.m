c1cbee146b484272c83e824583785f142b348636
Sergey D <sergey.dankevich@applifier.com>
2014-03-11 18:07:21 +0200

    caching manager tests
    testCacheEmptyCampaign added

--- ios/UnityAdsTests/UnityAdsCacheManagerTests.m
+++ ios/UnityAdsTests/UnityAdsCacheManagerTests.m
@@ -10,13 +10,36 @@
 #import "UnityAdsCampaign.h"
 #import "UnityAdsCacheManager.h"
 
-@interface UnityAdsCacheManagerTests : SenTestCase <UnityAdsCacheManagerDelegate>
+typedef enum {
+  CachingResultUndefined = 0,
+  CachingResultFinished,
+  CachingResultFailed,
+  CachingResultCancelled,
+} CachingResult;
+
+@interface UnityAdsCacheManagerTests : SenTestCase <UnityAdsCacheManagerDelegate> {
+  @private
+  CachingResult cachingResult;
+}
 
 @end
 
 extern void __gcov_flush();
 
 @implementation UnityAdsCacheManagerTests
+
+- (void)threadBlocked:(BOOL (^)())isThreadBlocked {
+	@autoreleasepool {
+		NSPort *port = [[NSPort alloc] init];
+		[port scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+		
+		while(isThreadBlocked) {
+			@autoreleasepool {
+				[[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate dateWithTimeIntervalSinceNow:0.5]];
+			}
+		}
+	}
+}
 
 - (void)setUp
 {
@@ -30,17 +53,30 @@ extern void __gcov_flush();
 }
 
 - (void)testCacheEmptyCampaign {
+  cachingResult = CachingResultUndefined;
   UnityAdsCacheManager * cacheManager = [UnityAdsCacheManager new];
   UnityAdsCampaign * campaignToCache = [UnityAdsCampaign new];
   [cacheManager cacheCampaign:campaignToCache];
+  STAssertTrue(cachingResult == CachingResultFailed,
+               @"caching should fail instantly in same thread when caching empty campaign");
 }
 
 - (void)cache:(UnityAdsCacheManager *)cache failedToCacheCampaign:(UnityAdsCampaign *)campaign {
-  
+  @synchronized(self) {
+    cachingResult = CachingResultFailed;
+  }
 }
 
 - (void)cache:(UnityAdsCacheManager *)cache finishedCachingCampaign:(UnityAdsCampaign *)campaign {
-  
+  @synchronized(self) {
+    cachingResult = CachingResultFinished;
+  }
+}
+
+- (void)cache:(UnityAdsCacheManager *)cache cancelledCaching:(UnityAdsCampaign *)campaign {
+  @synchronized(self) {
+    cachingResult = CachingResultCancelled;
+  }
 }
 
 @end
