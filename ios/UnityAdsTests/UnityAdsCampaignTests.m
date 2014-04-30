9cf96e8022da80f6177f27a9f377fef316604b10
Sergey D <sergd2005@gmail.com>
2014-04-30 15:48:41 +0300

    - removed check
    - added basic test

---
+++ ios/UnityAdsTests/UnityAdsCampaignTests.m
@@ -0,0 +1,56 @@
+//
+//  UnityAdsCampaignTests.m
+//  UnityAds
+//
+//  Created by Sergey D on 4/30/14.
+//  Copyright (c) 2014 Unity Technologies. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "NSObject+UnityAdsSBJson.h"
+#import "UnityAdsCampaign.h"
+#import "UnityAdsConstants.h"
+#import "UnityAds.h"
+
+@interface UnityAdsCampaignTests : SenTestCase
+
+@end
+
+@implementation UnityAdsCampaignTests
+
+- (void)setUp
+{
+  [[UnityAds sharedInstance] setDebugMode:YES];
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testCampaignInit {
+  NSError * error = nil;
+  NSStringEncoding encoding = NSStringEncodingConversionAllowLossy;
+  NSString * pathToResource = [[NSBundle bundleForClass:[self class]] pathForResource:@"jsonData.txt" ofType:nil];
+  NSString * jsonString = [[NSString alloc] initWithContentsOfFile:pathToResource
+                                                      usedEncoding:&encoding
+                                                             error:&error];
+  NSDictionary * jsonDataDictionary = [jsonString JSONValue];
+  NSDictionary * jsonDictionary = [jsonDataDictionary objectForKey:kUnityAdsJsonDataRootKey];
+  NSArray  * campaignsDataArray = [jsonDictionary objectForKey:kUnityAdsCampaignsKey];
+  NSMutableArray *campaigns = [NSMutableArray array];
+	
+	for (id campaignDictionary in campaignsDataArray) {
+		if ([campaignDictionary isKindOfClass:[NSDictionary class]]) {
+			UnityAdsCampaign *campaign = [[UnityAdsCampaign alloc] initWithData:campaignDictionary];
+      if (campaign.isValidCampaign) {
+        [campaigns addObject:campaign];
+      }
+		}
+	}  
+}
+
+@end
