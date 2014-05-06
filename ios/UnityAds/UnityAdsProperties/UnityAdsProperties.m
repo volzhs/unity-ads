4596f68dbda5001a5a8caf5816c4eab321bd5d2b
Ville Orkas <ville.orkas@applifier.com>
2014-05-06 12:21:50 +0300

    Bump version

--- ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
+++ ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
@@ -11,7 +11,7 @@
 #import "../UnityAds.h"
 #import "../UnityAdsDevice/UnityAdsDevice.h"
 
-NSString * const kUnityAdsVersion = @"1200";
+NSString * const kUnityAdsVersion = @"1202";
 
 @implementation UnityAdsProperties
 
