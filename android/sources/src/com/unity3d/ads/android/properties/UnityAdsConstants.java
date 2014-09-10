821619fc0a1d234e99376e1f976cb81f02600eaa
Janne Nikula <janne.nikula@applifier.com>
2014-09-10 13:34:56 +0300

    Release 1.3.8

--- android/sources/src/com/unity3d/ads/android/properties/UnityAdsConstants.java
+++ android/sources/src/com/unity3d/ads/android/properties/UnityAdsConstants.java
@@ -13,7 +13,7 @@ public class UnityAdsConstants {
 	 */
 
 	/* Unity Ads */
-	public static final String UNITY_ADS_VERSION = "1307";
+	public static final String UNITY_ADS_VERSION = "1308";
 	public static final String UNITY_ADS_REQUEST_METHOD_POST = "POST";
 	public static final String UNITY_ADS_REQUEST_METHOD_GET = "GET";
 
--- ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
+++ ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
@@ -11,7 +11,7 @@
 #import "../UnityAds.h"
 #import "../UnityAdsDevice/UnityAdsDevice.h"
 
-NSString * const kUnityAdsVersion = @"1307";
+NSString * const kUnityAdsVersion = @"1308";
 
 @implementation UnityAdsProperties
 
