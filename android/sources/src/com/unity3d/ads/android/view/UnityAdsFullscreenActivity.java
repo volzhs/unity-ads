070095368d57dc3d62936c92d8664c93d57a75c6
Janne Nikula <janne.nikula@applifier.com>
2014-10-21 12:15:13 +0300

    Add debug logging to activity lifecycle events

--- android/sources/src/com/unity3d/ads/android/view/UnityAdsFullscreenActivity.java
+++ android/sources/src/com/unity3d/ads/android/view/UnityAdsFullscreenActivity.java
@@ -8,30 +8,54 @@ import com.unity3d.ads.android.UnityAds;
 import com.unity3d.ads.android.UnityAdsDeviceLog;
 
 public class UnityAdsFullscreenActivity extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
 		UnityAdsDeviceLog.entered();   	
-        super.onCreate(savedInstanceState);
-   		UnityAds.changeActivity(this);
-    }
-    
-    @Override
-    public void onResume () {
+		super.onCreate(savedInstanceState);
+		UnityAds.changeActivity(this);
+	}
+
+	@Override
+	public void onStart() {
+		UnityAdsDeviceLog.entered();
+		super.onStart();
+	}
+
+	@Override
+	public void onRestart() {
+		UnityAdsDeviceLog.entered();
+		super.onRestart();
+	}
+
+	@Override
+	public void onResume() {
 		UnityAdsDeviceLog.entered();   	
-    	super.onResume();
-   		UnityAds.changeActivity(this);
+		super.onResume();
+		UnityAds.changeActivity(this);
    		UnityAds.checkMainview();
-    }
-    
-    @Override
+	}
+
+	@Override
+	public void onPause() {
+		UnityAdsDeviceLog.entered();
+		super.onPause();
+	}
+
+	@Override
+	public void onStop() {
+		UnityAdsDeviceLog.entered();
+		super.onStop();
+	}
+
+	@Override
 	protected void onDestroy() {
 		UnityAdsDeviceLog.entered();   	
-    	super.onDestroy();
-    	UnityAds.handleFullscreenDestroy();
+		super.onDestroy();
+		UnityAds.handleFullscreenDestroy();
 	}
-    
+
 	@Override
-    public boolean onKeyDown(int keyCode, KeyEvent event)  {
-    	return false;
-    }
+	public boolean onKeyDown(int keyCode, KeyEvent event)  {
+		return false;
+	}
 }
