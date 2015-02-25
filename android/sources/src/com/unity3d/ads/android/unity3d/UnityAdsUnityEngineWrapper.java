791c177c698127215e4044f46166df7f5a70ccc1
Ville Orkas <villeo@unity3d.com>
2015-02-25 16:40:58 +0200

    Final engine wrapper

--- android/sources/src/com/unity3d/ads/android/unity3d/UnityAdsUnityEngineWrapper.java
+++ android/sources/src/com/unity3d/ads/android/unity3d/UnityAdsUnityEngineWrapper.java
@@ -2,56 +2,27 @@ package com.unity3d.ads.android.unity3d;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
 import android.os.Build;
-import android.provider.Settings.Secure;
-import android.util.DisplayMetrics;
 
 import com.unity3d.ads.android.IUnityAdsListener;
 import com.unity3d.ads.android.UnityAds;
 import com.unity3d.ads.android.UnityAdsDeviceLog;
 import com.unity3d.ads.android.UnityAdsUtils;
-import com.unity3d.ads.android.data.UnityAdsDevice;
 
-import java.lang.reflect.Field;
 import java.util.HashMap;
 
 @TargetApi(Build.VERSION_CODES.GINGERBREAD)
 public class UnityAdsUnityEngineWrapper implements IUnityAdsListener {
-  private Activity _startupActivity = null;
-  private String _gameId = null;
-  private boolean _testMode = false;
   private static Boolean _initialized = false;
 
   public UnityAdsUnityEngineWrapper () {
-    try {
-      Class unityPlayer = Class.forName("com.unity3d.player.UnityPlayer");
-      Field currentActivityField = unityPlayer.getField("currentActivity");
-      Activity currentActivity = (Activity)currentActivityField.get(null);
-      if(currentActivity != null) {
-        _startupActivity = currentActivity;
-      }
-    } catch(Exception e) {}
   }
 
   // Public methods
 
-  public boolean isSupported () {
-    return UnityAds.isSupported();
-  }
-
-  public String getSDKVersion () {
-    return UnityAds.getSDKVersion();
-  }
-
-  public void init (final String gameId, final Activity activity, boolean testMode, final int logLevel) {
+  public void init (final String gameId, final Activity activity, final boolean testMode, final int logLevel) {
     if (!_initialized) {
       _initialized = true;
-      _gameId = gameId;
-      _testMode = testMode;
-
-      if (_startupActivity == null)
-        _startupActivity = activity;
 
       final UnityAdsUnityEngineWrapper listener = this;
 
@@ -60,8 +31,8 @@ public class UnityAdsUnityEngineWrapper implements IUnityAdsListener {
           @Override
           public void run() {
             UnityAdsDeviceLog.setLogLevel(logLevel);
-            UnityAds.setTestMode(_testMode);
-            UnityAds.init(_startupActivity, _gameId, listener);
+            UnityAds.setTestMode(testMode);
+            UnityAds.init(activity, gameId, listener);
           }
         });
       }
@@ -97,81 +68,8 @@ public class UnityAdsUnityEngineWrapper implements IUnityAdsListener {
     return false;
   }
 
-  public void hide () {
-    UnityAds.hide();
-  }
-
   public boolean canShowAds () {
     return UnityAds.canShowAds();
-  }
-
-  public boolean canShow () {
-    return UnityAds.canShow();
-  }
-
-  public boolean hasMultipleRewardItems () {
-    return UnityAds.hasMultipleRewardItems();
-  }
-
-  public String getRewardItemKeys () {
-    if (UnityAds.getRewardItemKeys() == null) return null;
-    if (UnityAds.getRewardItemKeys().size() > 0) {
-      String keys = "";
-      for (String key : UnityAds.getRewardItemKeys()) {
-        if (UnityAds.getRewardItemKeys().indexOf(key) > 0) {
-          keys += ";";
-        }
-        keys += key;
-      }
-
-      return keys;
-    }
-
-    return null;
-  }
-
-  public String getDefaultRewardItemKey () {
-    return UnityAds.getDefaultRewardItemKey();
-  }
-
-  public String getCurrentRewardItemKey () {
-    return UnityAds.getCurrentRewardItemKey();
-  }
-
-  public boolean setRewardItemKey (String rewardItemKey) {
-    return UnityAds.setRewardItemKey(rewardItemKey);
-  }
-
-  public void setDefaultRewardItemAsRewardItem () {
-    UnityAds.setDefaultRewardItemAsRewardItem();
-  }
-
-  public String getRewardItemDetailsWithKey (String rewardItemKey) {
-    String retString = "";
-
-    if (UnityAds.getRewardItemDetailsWithKey(rewardItemKey) != null) {
-      UnityAdsDeviceLog.debug("Fetching reward data");
-
-      @SuppressWarnings({ "unchecked", "rawtypes" })
-      HashMap<String, String> rewardMap = (HashMap)UnityAds.getRewardItemDetailsWithKey(rewardItemKey);
-
-      if (rewardMap != null) {
-        retString = rewardMap.get(UnityAds.UNITY_ADS_REWARDITEM_NAME_KEY);
-        retString += ";" + rewardMap.get(UnityAds.UNITY_ADS_REWARDITEM_PICTURE_KEY);
-        return retString;
-      }
-      else {
-        UnityAdsDeviceLog.debug("Problems getting reward item details");
-      }
-    }
-    else {
-      UnityAdsDeviceLog.debug("Could not find reward item details");
-    }
-    return "";
-  }
-
-  public String getRewardItemDetailsKeys () {
-    return String.format("%s;%s", UnityAds.UNITY_ADS_REWARDITEM_NAME_KEY, UnityAds.UNITY_ADS_REWARDITEM_PICTURE_KEY);
   }
 
   public void setLogLevel(int logLevel) {
@@ -219,65 +117,6 @@ public class UnityAdsUnityEngineWrapper implements IUnityAdsListener {
   @Override
   public void onFetchFailed() {
     UnityAdsOnFetchFailed();
-  }
-
-  // Device Info Wrapper
-
-  public String getPlatformName() {
-    return "android";
-  }
-
-  public String getAdvertisingIdentifier() {
-    return UnityAdsDevice.getAdvertisingTrackingId();
-  }
-
-  public boolean getNoTrack() {
-    return UnityAdsDevice.isLimitAdTrackingEnabled();
-  }
-
-  public String getVendor() {
-    return Build.MANUFACTURER;
-  }
-
-  public String getModel() {
-    return Build.MODEL;
-  }
-
-  public String getOSVersion() {
-    return Build.VERSION.RELEASE;
-  }
-
-  public String getDeviceId() {
-    try {
-      String androidId = Secure.getString(_startupActivity.getContentResolver(), Secure.ANDROID_ID);
-      return androidId;
-    } catch (Exception e) {
-      return null;
-    }
-  }
-
-  public String getBundleId() {
-    Context context = _startupActivity.getApplicationContext();
-    if (context != null) {
-      return context.getPackageName();
-    }
-    return null;
-  }
-
-  public String getScreenSize() {
-    if (_startupActivity== null) return String.valueOf(-1);
-    DisplayMetrics metrics = new DisplayMetrics();
-    _startupActivity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-    double x = metrics.widthPixels / metrics.xdpi;
-    double y = metrics.heightPixels / metrics.ydpi;
-    return String.valueOf(Math.sqrt(x * x + y * y));
-  }
-
-  public String getScreenDpi() {
-    if (_startupActivity == null) return String.valueOf(-1);
-    DisplayMetrics metrics = new DisplayMetrics();
-    _startupActivity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
-    return String.valueOf(metrics.densityDpi);
   }
 
 }
