cea67601ffeb60271dea753c3d346e275f5a3d7d
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-05 11:28:44 +0300

    Cleanup unused imports and variables from videopausedview

--- android/sources/src/com/unity3d/ads/android/video/UnityAdsVideoPausedView.java
+++ android/sources/src/com/unity3d/ads/android/video/UnityAdsVideoPausedView.java
@@ -1,32 +1,13 @@
 package com.unity3d.ads.android.video;
 
-import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Canvas.VertexMode;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Paint.Style;
-import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import com.unity3d.ads.android.R;
 
 public class UnityAdsVideoPausedView extends RelativeLayout {
-	private ImageView _outerStroke = null;
-
-	private static float screenDensity;
-	
 	public UnityAdsVideoPausedView(Context context) {
 		super(context);
 		createView();
