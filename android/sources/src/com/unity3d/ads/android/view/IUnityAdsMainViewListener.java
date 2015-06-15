4c9a7da6e827b733fc8e23da7e4f20ac761c0e2c
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-15 16:30:39 +0300

    Remove listener interface

--- android/sources/src/com/unity3d/ads/android/view/IUnityAdsMainViewListener.java
+++
@@ -1,7 +0,0 @@
-package com.unity3d.ads.android.view;
-
-import com.unity3d.ads.android.view.UnityAdsMainView.UnityAdsMainViewAction;
-
-public interface IUnityAdsMainViewListener {
-	public void onMainViewAction (UnityAdsMainViewAction action);
-}
