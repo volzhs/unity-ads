37a749c3a6a56e1234443bc6b6dddd8109b99c3b
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-17 11:54:14 +0300

    Cleanup

--- android/sources/src/com/unity3d/ads/android/view/UnityAdsMainView.java
+++ android/sources/src/com/unity3d/ads/android/view/UnityAdsMainView.java
@@ -6,7 +6,6 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 
@@ -24,8 +23,8 @@ import com.unity3d.ads.android.webapp.IUnityAdsWebViewListener;
 @TargetApi(Build.VERSION_CODES.GINGERBREAD)
 public class UnityAdsMainView extends RelativeLayout {
 
-	public static enum UnityAdsMainViewState { WebView, VideoPlayer };
-	public static enum UnityAdsMainViewAction { VideoStart, VideoEnd, VideoSkipped, BackButtonPressed };
+	public enum UnityAdsMainViewState { WebView, VideoPlayer }
+	public enum UnityAdsMainViewAction { VideoStart, VideoEnd, VideoSkipped, BackButtonPressed }
 	private static final int FILL_PARENT = -1;
 	
 	// Views
@@ -93,78 +92,73 @@ public class UnityAdsMainView extends RelativeLayout {
 			webview = null;
 		}
 
-		if (webview == null) {
-			UnityAdsDeviceLog.debug("Initing WebView");
+		UnityAdsDeviceLog.debug("Initing WebView");
 
-			webview = new UnityAdsWebView(UnityAdsProperties.APPLICATION_CONTEXT.get(), new IUnityAdsWebViewListener() {
-				@Override
-				public void onWebAppLoaded() {
-					webview.initWebApp(UnityAds.webdata.getData());
-				}
-			}, new UnityAdsWebBridge(new IUnityAdsWebBridgeListener() {
-				@Override
-				public void onPlayVideo(JSONObject data) { }
+		webview = new UnityAdsWebView(UnityAdsProperties.APPLICATION_CONTEXT.get(), new IUnityAdsWebViewListener() {
+			@Override
+			public void onWebAppLoaded() {
+				webview.initWebApp(UnityAds.webdata.getData());
+			}
+		}, new UnityAdsWebBridge(new IUnityAdsWebBridgeListener() {
+			@Override
+			public void onPlayVideo(JSONObject data) { }
 
-				@Override
-				public void onPauseVideo(JSONObject data) {	}
+			@Override
+			public void onPauseVideo(JSONObject data) {	}
 
-				@Override
-				public void onCloseAdsView(JSONObject data) { }
+			@Override
+			public void onCloseAdsView(JSONObject data) { }
 
-				@Override
-				public void onWebAppLoadComplete(JSONObject data) {	}
+			@Override
+			public void onWebAppLoadComplete(JSONObject data) {	}
 
-				@Override
-				public void onWebAppInitComplete(JSONObject data) {
-					UnityAdsDeviceLog.entered();
-					Boolean dataOk = true;
+			@Override
+			public void onWebAppInitComplete(JSONObject data) {
+				UnityAdsDeviceLog.entered();
+				Boolean dataOk = true;
 
-					if(UnityAds.webdata != null && UnityAds.webdata.hasViewableAds()) {
-						JSONObject setViewData = new JSONObject();
+				if(UnityAds.webdata != null && UnityAds.webdata.hasViewableAds()) {
+					JSONObject setViewData = new JSONObject();
 
-						try {
-							setViewData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_API_ACTION_KEY, UnityAdsConstants.UNITY_ADS_WEBVIEW_API_INITCOMPLETE);
-						}
-						catch (Exception e) {
-							dataOk = false;
-						}
+					try {
+						setViewData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_API_ACTION_KEY, UnityAdsConstants.UNITY_ADS_WEBVIEW_API_INITCOMPLETE);
+					}
+					catch (Exception e) {
+						dataOk = false;
+					}
 
-						if (dataOk) {
-							UnityAdsMainView.webview.setWebViewCurrentView(UnityAdsConstants.UNITY_ADS_WEBVIEW_VIEWTYPE_NONE, setViewData);
+					if (dataOk) {
+						UnityAdsMainView.webview.setWebViewCurrentView(UnityAdsConstants.UNITY_ADS_WEBVIEW_VIEWTYPE_NONE, setViewData);
 
-							UnityAdsUtils.runOnUiThread(new Runnable() {
-								@Override
-								public void run() {
-									if (!UnityAdsProperties.isAdsReadySent() && UnityAds.getListener() != null) {
-										UnityAdsDeviceLog.debug("Unity Ads ready.");
-										UnityAds.getListener().onFetchCompleted();
-										UnityAdsProperties.setAdsReadySent(true);
-									}
+						UnityAdsUtils.runOnUiThread(new Runnable() {
+							@Override
+							public void run() {
+								if (!UnityAdsProperties.isAdsReadySent() && UnityAds.getListener() != null) {
+									UnityAdsDeviceLog.debug("Unity Ads ready.");
+									UnityAds.getListener().onFetchCompleted();
+									UnityAdsProperties.setAdsReadySent(true);
 								}
-							});
-						}
+							}
+						});
 					}
 				}
+			}
 
-				@Override
-				public void onOrientationRequest(JSONObject data) {	}
+			@Override
+			public void onOrientationRequest(JSONObject data) {	}
 
-				@Override
-				public void onOpenPlayStore(JSONObject data) { }
+			@Override
+			public void onOpenPlayStore(JSONObject data) { }
 
-				@Override
-				public void onLaunchIntent(JSONObject data) { }
-			}));
-			webview.setId(1003);
-		}
+			@Override
+			public void onLaunchIntent(JSONObject data) { }
+		}));
 	}
 	
 	/* PRIVATE METHODS */
 
 	private void init () {
 		UnityAdsDeviceLog.entered();   	
-		this.setId(1001);
-
 		webview.setWebBridgeListener(_webBridgeListener);
 
 		post(new Runnable() {
@@ -188,7 +182,6 @@ public class UnityAdsMainView extends RelativeLayout {
 	private void createVideoPlayerView () {
 		videoplayerview = new UnityAdsVideoPlayView(getContext());
 		videoplayerview.setLayoutParams(new FrameLayout.LayoutParams(FILL_PARENT, FILL_PARENT));
-		videoplayerview.setId(1002);
 		addView(videoplayerview);
 	}
 	
