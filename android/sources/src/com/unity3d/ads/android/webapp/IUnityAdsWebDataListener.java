a86e323cbe5879186bd1f8cdf9aea647ed791d14
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-23 16:39:50 +0300

    Fix LINT warnings from IUnityAdsWebDataListener

--- android/sources/src/com/unity3d/ads/android/webapp/IUnityAdsWebDataListener.java
+++ android/sources/src/com/unity3d/ads/android/webapp/IUnityAdsWebDataListener.java
@@ -1,6 +1,6 @@
 package com.unity3d.ads.android.webapp;
 
 public interface IUnityAdsWebDataListener {
-	public void onWebDataCompleted ();
-	public void onWebDataFailed ();
+	void onWebDataCompleted ();
+	void onWebDataFailed ();
 }
