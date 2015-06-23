daea5615b9076c602e6623fa633ed5e5429f3193
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-23 16:08:02 +0300

    Cleanup and fix LINT warnings from IUnityAdsVideoPlayerListener

--- android/sources/src/com/unity3d/ads/android/video/IUnityAdsVideoPlayerListener.java
+++ android/sources/src/com/unity3d/ads/android/video/IUnityAdsVideoPlayerListener.java
@@ -5,12 +5,9 @@ import android.media.MediaPlayer.OnCompletionListener;
 import com.unity3d.ads.android.view.IUnityAdsViewListener;
 import com.unity3d.ads.android.webapp.UnityAdsWebData.UnityAdsVideoPosition;
 
-
-public interface IUnityAdsVideoPlayerListener extends IUnityAdsViewListener,
-		OnCompletionListener {
-	
-	public void onEventPositionReached (UnityAdsVideoPosition position);
-	public void onVideoPlaybackStarted ();
-	public void onVideoPlaybackError ();
-	public void onVideoSkip ();
+public interface IUnityAdsVideoPlayerListener extends IUnityAdsViewListener, OnCompletionListener {
+	void onEventPositionReached (UnityAdsVideoPosition position);
+	void onVideoPlaybackStarted ();
+	void onVideoPlaybackError ();
+	void onVideoSkip ();
 }
