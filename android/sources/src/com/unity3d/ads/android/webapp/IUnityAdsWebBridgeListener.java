d437294bcf88b3d1d260ab025061b3f6186e99f8
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-23 16:24:52 +0300

    Fix LINT warnings from IUnityAdsWebBridgeListener

--- android/sources/src/com/unity3d/ads/android/webapp/IUnityAdsWebBridgeListener.java
+++ android/sources/src/com/unity3d/ads/android/webapp/IUnityAdsWebBridgeListener.java
@@ -3,12 +3,12 @@ package com.unity3d.ads.android.webapp;
 import org.json.JSONObject;
 
 public interface IUnityAdsWebBridgeListener {
-	public void onPlayVideo (JSONObject data);
-	public void onPauseVideo (JSONObject data);
-	public void onCloseAdsView (JSONObject data);
-	public void onWebAppLoadComplete (JSONObject data);
-	public void onWebAppInitComplete (JSONObject data);
-	public void onOrientationRequest (JSONObject data);
-	public void onOpenPlayStore (JSONObject data);
-	public void onLaunchIntent(JSONObject data);
+	void onPlayVideo (JSONObject data);
+	void onPauseVideo (JSONObject data);
+	void onCloseAdsView (JSONObject data);
+	void onWebAppLoadComplete (JSONObject data);
+	void onWebAppInitComplete (JSONObject data);
+	void onOrientationRequest (JSONObject data);
+	void onOpenPlayStore (JSONObject data);
+	void onLaunchIntent(JSONObject data);
 }
