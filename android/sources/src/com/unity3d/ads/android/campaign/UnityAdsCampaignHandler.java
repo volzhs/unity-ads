6a15004da55d5a21660632015a1a14e85e7ac340
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-23 15:20:25 +0300

    Cleanup and fix LINT warnings from UnityAdsCampaignHandler

--- android/sources/src/com/unity3d/ads/android/campaign/UnityAdsCampaignHandler.java
+++ android/sources/src/com/unity3d/ads/android/campaign/UnityAdsCampaignHandler.java
@@ -8,15 +8,10 @@ import com.unity3d.ads.android.cache.UnityAdsDownloader;
 import com.unity3d.ads.android.cache.IUnityAdsDownloadListener;
 
 public class UnityAdsCampaignHandler implements IUnityAdsDownloadListener {
-	
 	private ArrayList<String> _downloadList = null;
 	private UnityAdsCampaign _campaign = null;
 	private IUnityAdsCampaignHandlerListener _handlerListener = null;
-	private long _cacheStartMillis = 0;
-	private long _cacheSolvedMillis = 0;
-	//private boolean _cancelledDownloads = false;
-	
-	
+
 	public UnityAdsCampaignHandler (UnityAdsCampaign campaign) {
 		_campaign = campaign;
 	}
@@ -28,37 +23,28 @@ public class UnityAdsCampaignHandler implements IUnityAdsDownloadListener {
 	public UnityAdsCampaign getCampaign () {
 		return _campaign;
 	}
-	
-	public long getCachingDurationInMillis () {
-		if (_cacheStartMillis > 0 && _cacheSolvedMillis > 0) {
-			return _cacheSolvedMillis - _cacheStartMillis;
-		}
-		
-		return 0;
-	}
-	
+
 	public void setListener (IUnityAdsCampaignHandlerListener listener) {
 		_handlerListener = listener;
 	}
-	
+
 	@Override
 	public void onFileDownloadCompleted (String downloadUrl) {
 		if (finishDownload(downloadUrl)) {
 			UnityAdsDeviceLog.debug("Reporting campaign download completion: " + _campaign.getCampaignId());
 		}
 	}
-	
+
 	@Override
 	public void onFileDownloadCancelled (String downloadUrl) {	
 		if (finishDownload(downloadUrl)) {
 			UnityAdsDeviceLog.debug("Download cancelled: " + _campaign.getCampaignId());
 		}
 	}
-	
+
 	public void initCampaign(boolean firstInAdPlan) {
-		// Check video
-		checkFileAndDownloadIfNeeded(_campaign.getVideoUrl(), firstInAdPlan);
-		
+		checkFileAndDownloadIfNeeded(_campaign, firstInAdPlan);
+
 		if (_handlerListener != null) {
 			_handlerListener.onCampaignHandled(this);
 		}
@@ -68,10 +54,10 @@ public class UnityAdsCampaignHandler implements IUnityAdsDownloadListener {
 		if (!UnityAdsUtils.isFileInCache(_campaign.getVideoFilename()) && UnityAdsUtils.canUseExternalStorage()) {
 			if (!hasDownloads())
 				UnityAdsDownloader.addListener(this);
-			
+
 			addCampaignToDownloads();			
 		}
-		else if (!isFileOk(_campaign.getVideoFilename()) && UnityAdsUtils.canUseExternalStorage()) {
+		else if (!isCampaignFileOk(_campaign) && UnityAdsUtils.canUseExternalStorage()) {
 			if (!hasDownloads())
 				UnityAdsDownloader.addListener(this);
 
@@ -81,89 +67,62 @@ public class UnityAdsCampaignHandler implements IUnityAdsDownloadListener {
 		}		
 	}
 
-	public void clearData () {
-		if (_handlerListener != null)
-			_handlerListener = null;
-		
-		if (_downloadList != null) {
-			_downloadList.clear();
-		}
-		
-		if (_campaign != null) {
-			_campaign.clearData();
-			_campaign = null;
-		}
-	}
-	
-	
 	/* INTERNAL METHODS */
-	
+
 	private boolean finishDownload (String downloadUrl) {
-		_cacheSolvedMillis = System.currentTimeMillis();
 		removeDownload(downloadUrl);
 		
 		if (_downloadList != null && _downloadList.size() == 0 && _handlerListener != null) {
 			UnityAdsDownloader.removeListener(this);
 			return true;
 		}
-		
+
 		return false;
 	}
 
-	private void checkFileAndDownloadIfNeeded(String fileUrl, boolean firstInAdPlan) {
-		if((_campaign.shouldCacheVideo() || (_campaign.allowCacheVideo() && firstInAdPlan)) && !UnityAdsUtils.isFileInCache(_campaign.getVideoFilename()) && UnityAdsUtils.canUseExternalStorage()) {
+	private void checkFileAndDownloadIfNeeded(UnityAdsCampaign campaign, boolean firstInAdPlan) {
+		if((campaign.shouldCacheVideo() || (campaign.allowCacheVideo() && firstInAdPlan)) && !UnityAdsUtils.isFileInCache(campaign.getVideoFilename()) && UnityAdsUtils.canUseExternalStorage()) {
 			if (!hasDownloads())
 				UnityAdsDownloader.addListener(this);
 
 			addCampaignToDownloads();			
 		}
-		else if (_campaign.shouldCacheVideo() && !isFileOk(fileUrl) && UnityAdsUtils.canUseExternalStorage()) {
+		else if (campaign.shouldCacheVideo() && !isCampaignFileOk(campaign) && UnityAdsUtils.canUseExternalStorage()) {
 			UnityAdsDeviceLog.debug("The file was not okay, redownloading");
-			UnityAdsUtils.removeFile(_campaign.getVideoFilename());
+			UnityAdsUtils.removeFile(campaign.getVideoFilename());
 			UnityAdsDownloader.addListener(this);
 			addCampaignToDownloads();
 		}		
 	}
-	
-	private boolean isFileOk (String fileUrl) {
-		long localSize = UnityAdsUtils.getSizeForLocalFile(_campaign.getVideoFilename());
-		long expectedSize = _campaign.getVideoFileExpectedSize();
-		
+
+	private boolean isCampaignFileOk(UnityAdsCampaign campaign) {
+		long localSize = UnityAdsUtils.getSizeForLocalFile(campaign.getVideoFilename());
+		long expectedSize = campaign.getVideoFileExpectedSize();
+
 		UnityAdsDeviceLog.debug("localSize=" + localSize + ", expectedSize=" + expectedSize);
-				
-		if (localSize == -1)
-			return false;
-		
-		if (expectedSize == -1)
-			return true;
-		
-		if (localSize > 0 && expectedSize > 0 && localSize == expectedSize)
-			return true;
-			
-		return false;
+		return localSize != -1 && (expectedSize == -1 || localSize > 0 && expectedSize > 0 && localSize == expectedSize);
 	}
-	
+
 	private void addCampaignToDownloads () {
 		if (_campaign == null) return;
-		if (_downloadList == null) _downloadList = new ArrayList<String>();
+		if (_downloadList == null) _downloadList = new ArrayList<>();
 
 		_downloadList.add(_campaign.getVideoUrl());
-		_cacheStartMillis = System.currentTimeMillis();
 		UnityAdsDownloader.addDownload(_campaign);
 	}
 
 	private void removeDownload (String downloadUrl) {
 		if (_downloadList == null) return;
-		
+
 		int removeIndex = -1;
-		
+
 		for (int i = 0; i < _downloadList.size(); i++) {
 			if (_downloadList.get(i).equals(downloadUrl)) {
 				removeIndex = i;
 				break;
 			}
 		}
-		
+
 		if (removeIndex > -1)
 			_downloadList.remove(removeIndex);
 	}
