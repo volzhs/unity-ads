453a3a4517a101b1a47ba872ce888fd6aa6b9998
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-23 14:49:46 +0300

    Fix LINT warnings from IUnityAdsCacheListener

--- android/sources/src/com/unity3d/ads/android/cache/IUnityAdsCacheListener.java
+++ android/sources/src/com/unity3d/ads/android/cache/IUnityAdsCacheListener.java
@@ -3,7 +3,7 @@ package com.unity3d.ads.android.cache;
 import com.unity3d.ads.android.campaign.UnityAdsCampaignHandler;
 
 public interface IUnityAdsCacheListener {
-	public void onCampaignUpdateStarted ();
-	public void onCampaignReady (UnityAdsCampaignHandler campaignHandler);
-	public void onAllCampaignsReady ();
+	void onCampaignUpdateStarted ();
+	void onCampaignReady (UnityAdsCampaignHandler campaignHandler);
+	void onAllCampaignsReady ();
 }
