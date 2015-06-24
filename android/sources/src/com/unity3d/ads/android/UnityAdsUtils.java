95e2e3d2cc211a67dd0c6afd71a29e2e95449f97
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-24 13:04:57 +0300

    Cleanup and fix LINT warnings from UnityAdsUtils

--- android/sources/src/com/unity3d/ads/android/UnityAdsUtils.java
+++ android/sources/src/com/unity3d/ads/android/UnityAdsUtils.java
@@ -5,7 +5,6 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
@@ -37,99 +36,86 @@ public class UnityAdsUtils {
 
 	private static final X500Principal DEBUG_DN = new X500Principal("CN=Android Debug,O=Android,C=US");
 	private static String _cacheDirectory = null;
-	
+
 	public static boolean isDebuggable(Context ctx) {
 	    boolean debuggable = false;
 	    boolean problemsWithData = false;
-	    
-	    if (ctx == null) return debuggable;
-	    
+
+	    if (ctx == null) return false;
+
 	    PackageManager pm = ctx.getPackageManager();
 	    try {
 	        ApplicationInfo appinfo = pm.getApplicationInfo(ctx.getPackageName(), 0);
 	        debuggable = (0 != (appinfo.flags &= ApplicationInfo.FLAG_DEBUGGABLE));
 	    }
 	    catch (NameNotFoundException e) {
+			UnityAdsDeviceLog.debug("Could not find name");
 	        problemsWithData = true;
 	    }
-	    
+
 	    if (problemsWithData) {
-	    	problemsWithData = false;
 		    try {
-		        PackageInfo pinfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(),PackageManager.GET_SIGNATURES);
+		        PackageInfo pinfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), PackageManager.GET_SIGNATURES);
 		        Signature signatures[] = pinfo.signatures;
-		         
-		        for ( int i = 0; i < signatures.length;i++) {
-		            CertificateFactory cf = CertificateFactory.getInstance("X.509");
-		            ByteArrayInputStream stream = new ByteArrayInputStream(signatures[i].toByteArray());
-		            X509Certificate cert = (X509Certificate) cf.generateCertificate(stream);       
-		            debuggable = cert.getSubjectX500Principal().equals(DEBUG_DN);
-		            if (debuggable)
-		                break;
-		        }
+
+				for (Signature signature : signatures) {
+					CertificateFactory cf = CertificateFactory.getInstance("X.509");
+					ByteArrayInputStream stream = new ByteArrayInputStream(signature.toByteArray());
+					X509Certificate cert = (X509Certificate) cf.generateCertificate(stream);
+					debuggable = cert.getSubjectX500Principal().equals(DEBUG_DN);
+					if (debuggable)
+						break;
+				}
 		    }
 		    catch (NameNotFoundException e) {
-		    	problemsWithData = true;
+				UnityAdsDeviceLog.debug("Could not find name");
 		    }
 		    catch (CertificateException e) {
-		    	problemsWithData = true;
+				UnityAdsDeviceLog.debug("Certificate exception");
 		    }
 	    }
-	    
+
 	    return debuggable;
 	}
-	
-    private static String convertToHex(byte[] data) {
-        StringBuilder buf = new StringBuilder();
-        for (byte b : data) {
-            int halfbyte = (b >>> 4) & 0x0F;
-            int two_halfs = 0;
-            do {
-                buf.append((0 <= halfbyte) && (halfbyte <= 9) ? (char) ('0' + halfbyte) : (char) ('a' + (halfbyte - 10)));
-                halfbyte = b & 0x0F;
-            } while (two_halfs++ < 1);
-        }
-        return buf.toString();
-    }
 
-    public static String SHA1(String text) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-        MessageDigest md = MessageDigest.getInstance("SHA-1");
-        md.update(text.getBytes("iso-8859-1"), 0, text.length());
-        byte[] sha1hash = md.digest();
-        return convertToHex(sha1hash);
-    }
-	
 	@SuppressLint("DefaultLocale")
 	public static String Md5 (String input) {
+		if (input == null) return null;
+
 		MessageDigest m = null;
 		try {
 			m = MessageDigest.getInstance("MD5");
 		} catch (NoSuchAlgorithmException e) {
 			e.printStackTrace();
 		}
-		m.update(input.getBytes(),0,input.length());
+
+		if (m == null) return null;
+
+		byte strData[] = input.getBytes();
+		int length = input.length();
+		m.update(strData,0,length);
 		byte p_md5Data[] = m.digest();
-		
-		String mOutput = new String();
-		for (int i=0;i < p_md5Data.length;i++) {
-			int b =  (0xFF & p_md5Data[i]);
+
+		String mOutput = "";
+		for (byte aP_md5Data : p_md5Data) {
+			int b = (0xFF & aP_md5Data);
 			// if it is a single digit, make sure it have 0 in front (proper padding)
-			if (b <= 0xF) mOutput+="0";
+			if (b <= 0xF) mOutput += "0";
 			// add number to string
-			mOutput+=Integer.toHexString(b);
+			mOutput += Integer.toHexString(b);
 		}
 		// hex string to uppercase
 		return mOutput.toUpperCase();
 	}
-	
+
 	public static String readFile (File fileToRead, boolean addLineBreaks) {
 		String fileContent = "";
-		BufferedReader br = null;
-		
+		BufferedReader br;
+
 		if (fileToRead.exists() && fileToRead.canRead()) {
 			try {
 				br = new BufferedReader(new FileReader(fileToRead));
-				String line = null;
+				String line;
 				
 				while ((line = br.readLine()) != null) {
 					fileContent = fileContent.concat(line);
@@ -141,14 +127,14 @@ public class UnityAdsUtils {
 				UnityAdsDeviceLog.error("Problem reading file: " + e.getMessage());
 				return null;
 			}
-			
+
 			try {
 				br.close();
 			}
 			catch (Exception e) {
 				UnityAdsDeviceLog.error("Problem closing reader: " + e.getMessage());
 			}
-						
+
 			return fileContent;
 		}
 		else {
@@ -157,10 +143,10 @@ public class UnityAdsUtils {
 		
 		return null;
 	}
-	
+
 	public static boolean writeFile (File fileToWrite, String content) {
-		FileOutputStream fos = null;
-		
+		FileOutputStream fos;
+
 		try {
 			fos = new FileOutputStream(fileToWrite);
 			fos.write(content.getBytes());
@@ -171,17 +157,17 @@ public class UnityAdsUtils {
 			UnityAdsDeviceLog.error("Could not write file: " + e.getMessage());
 			return false;
 		}
-		
+
 		UnityAdsDeviceLog.debug("Wrote file: " + fileToWrite.getAbsolutePath());
-		
+
 		return true;
 	}
-	
+
 	public static void removeFile (String fileName) {
 		if(fileName != null) {
 			File removeFile = new File (fileName);
 			File cachedVideoFile = new File (UnityAdsUtils.getCacheDirectory() + "/" + removeFile.getName());
-			
+
 			if (cachedVideoFile.exists()) {
 				if (!cachedVideoFile.delete())
 					UnityAdsDeviceLog.error("Could not delete: " + cachedVideoFile.getAbsolutePath());
@@ -193,16 +179,16 @@ public class UnityAdsUtils {
 			}
 		}
 	}
-	
+
 	public static long getSizeForLocalFile (String fileName) {
 		File removeFile = new File (fileName);
 		File cachedVideoFile = new File (UnityAdsUtils.getCacheDirectory() + "/" + removeFile.getName());
 		long size = -1;
-		
+
 		if (cachedVideoFile.exists()) {
 			size = cachedVideoFile.length();
 		}
-		
+
 		return size;
 	}
 
@@ -210,7 +196,7 @@ public class UnityAdsUtils {
 		File externalCacheDirectory = activity.getExternalCacheDir();
 
 		// If possible, use app private external cache directory. It requires no external storage permission for api level 19+ devices.
-		if(externalCacheDirectory != null && externalCacheDirectory.getAbsolutePath() != null) {
+		if(externalCacheDirectory != null) {
 			_cacheDirectory = externalCacheDirectory.getAbsolutePath() + "/" + UnityAdsConstants.CACHE_DIR_NAME;
 		} else {
 			_cacheDirectory = Environment.getExternalStorageDirectory() + "/" + UnityAdsConstants.CACHE_DIR_NAME;
@@ -220,40 +206,35 @@ public class UnityAdsUtils {
 	public static String getCacheDirectory () {
 		return _cacheDirectory;
 	}
-	
+
 	public static boolean canUseExternalStorage () {
 		String state = Environment.getExternalStorageState();
-		if (state.equals(Environment.MEDIA_MOUNTED))
-			return true;
-		
-		return false;
+		return state.equals(Environment.MEDIA_MOUNTED);
 	}
-	
+
 	public static File createCacheDir () {
 		File tdir = new File (getCacheDirectory());
-		tdir.mkdirs();
-		
-		if (tdir != null) {
+		if (tdir.mkdirs()) {
 			UnityAdsUtils.writeFile(new File(getCacheDirectory() + "/.nomedia"), "");
 		}
-		
+
 		return tdir;
 	}
-	
+
 	public static boolean isFileRequiredByCampaigns (String fileName, ArrayList<UnityAdsCampaign> campaigns) {
 		if (fileName == null || campaigns == null) return false;
-		
+
 		File seekFile = new File(fileName);
-		
+
 		if (seekFile.getName().equals(".nomedia"))
 			return true;
-		
+
 		for (UnityAdsCampaign campaign : campaigns) {
 			File matchFile = new File(campaign.getVideoFilename());
 			if (seekFile.getName().equals(matchFile.getName()))
 				return true;
 		}
-		
+
 		return false;
 	}
 
