6e9955007cda14c44d23a609663bd595c68bdfdd
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-24 12:38:07 +0300

    Cleanup and fix LINT warnings from IUnityAdsListener

--- android/sources/src/com/unity3d/ads/android/IUnityAdsListener.java
+++ android/sources/src/com/unity3d/ads/android/IUnityAdsListener.java
@@ -2,14 +2,14 @@ package com.unity3d.ads.android;
 
 public interface IUnityAdsListener {
 	// Unity Ads view events
-	public void onHide ();
-	public void onShow ();
-	
+	void onHide ();
+	void onShow ();
+
 	// Unity Ads video events
-	public void onVideoStarted ();
-	public void onVideoCompleted (String rewardItemKey, boolean skipped);
-	
+	void onVideoStarted ();
+	void onVideoCompleted (String rewardItemKey, boolean skipped);
+
 	// Unity Ads campaign events
-	public void onFetchCompleted ();
-	public void onFetchFailed ();
+	void onFetchCompleted ();
+	void onFetchFailed ();
 }
