4ae3e2e7817983b7fa0de34db5026efa17e3f390
Pekka Palmu <pekka.palmu@gmail.com>
2015-06-24 12:19:44 +0300

    Cleanup and fix LINT warnings from UnityAdsWebView

--- android/sources/src/com/unity3d/ads/android/webapp/UnityAdsWebView.java
+++ android/sources/src/com/unity3d/ads/android/webapp/UnityAdsWebView.java
@@ -5,10 +5,9 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Paint;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -60,7 +59,6 @@ public class UnityAdsWebView extends WebView {
 		init(url, listener, webBridge);
 	}
 
-	
 	public boolean isWebAppLoaded () {
 		return _webAppLoaded;
 	}
@@ -68,30 +66,30 @@ public class UnityAdsWebView extends WebView {
 	public void setWebViewCurrentView (String view, JSONObject data) {		
 		if (isWebAppLoaded()) {
 			String dataString = "{}";
-			
+
 			if (data != null)
 				dataString = data.toString();
-			
+
 			String javascriptString = String.format("%s%s(\"%s\", %s);", UnityAdsConstants.UNITY_ADS_WEBVIEW_JS_PREFIX, UnityAdsConstants.UNITY_ADS_WEBVIEW_JS_CHANGE_VIEW, view, dataString);
 			UnityAdsUtils.runOnUiThread(new UnityAdsJavascriptRunner(javascriptString, this));
 			UnityAdsDeviceLog.debug("Send change view to WebApp: " + javascriptString);
-			
+
 			if (data != null && UnityAdsProperties.getBaseActivity() != null) {
 				String action = "test";
 				try {
 					action = data.getString(UnityAdsConstants.UNITY_ADS_WEBVIEW_API_ACTION_KEY);
 				}
 				catch (Exception e) {
+					UnityAdsDeviceLog.debug("Couldn't get API action key: " + e.getMessage());
 				}
-				
+
 				UnityAdsDeviceLog.debug("dataHasApiActionKey=" + data.has(UnityAdsConstants.UNITY_ADS_WEBVIEW_API_ACTION_KEY));
 				UnityAdsDeviceLog.debug("actionEqualsWebViewApiOpen=" + action.equals(UnityAdsConstants.UNITY_ADS_WEBVIEW_API_OPEN));
 				UnityAdsDeviceLog.debug("isDebuggable=" + UnityAdsUtils.isDebuggable(UnityAdsProperties.getBaseActivity()));
 				UnityAdsDeviceLog.debug("runWebViewTests=" + UnityAdsProperties.RUN_WEBVIEW_TESTS);
 				UnityAdsDeviceLog.debug("testJavaScriptContents=" + UnityAdsProperties.TEST_JAVASCRIPT);
-				
+
 				if (data.has(UnityAdsConstants.UNITY_ADS_WEBVIEW_API_ACTION_KEY) &&
-					action != null &&
 					action.equals(UnityAdsConstants.UNITY_ADS_WEBVIEW_API_OPEN) &&
 					UnityAdsUtils.isDebuggable(UnityAdsProperties.getBaseActivity()) &&
 					UnityAdsProperties.RUN_WEBVIEW_TESTS &&
@@ -103,11 +101,11 @@ public class UnityAdsWebView extends WebView {
 			}
 		}
 	}
-	
+
 	public void sendNativeEventToWebApp (String eventType, JSONObject data) {
 		if (isWebAppLoaded()) {
 			String dataString = "{}";
-			
+
 			if (data != null)
 				dataString = data.toString();
 
@@ -116,7 +114,7 @@ public class UnityAdsWebView extends WebView {
 			UnityAdsUtils.runOnUiThread(new UnityAdsJavascriptRunner(javascriptString, this));
 		}
 	}
-	
+
 	public void initWebApp (JSONObject data) {
 		if (isWebAppLoaded()) {
 			JSONObject initData = new JSONObject();
@@ -125,15 +123,15 @@ public class UnityAdsWebView extends WebView {
 				// Basic data
 				initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_CAMPAIGNDATA_KEY, data);
 				initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_PLATFORM_KEY, "android");
-
 				String advertisingId = UnityAdsDevice.getAdvertisingTrackingId();
-				if(advertisingId != null) {
-					initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_TRACKINGENABLED_KEY, UnityAdsDevice.isLimitAdTrackingEnabled() ? 0 : 1);
 
+				if (advertisingId != null) {
+					initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_TRACKINGENABLED_KEY, UnityAdsDevice.isLimitAdTrackingEnabled() ? 0 : 1);
 					String advertisingIdMd5 = UnityAdsUtils.Md5(advertisingId).toLowerCase();
 					initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_ADVERTISINGTRACKINGID_KEY, advertisingIdMd5);
 					initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_RAWADVERTISINGTRACKINGID_KEY, advertisingId);
-				} else {
+				}
+				else {
 					if (!UnityAdsConstants.UNITY_ADS_DEVICEID_UNKNOWN.equals(UnityAdsDevice.getAndroidId(false))) {
 						initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_ANDROIDID_KEY, UnityAdsDevice.getAndroidId(true));
 						initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_RAWANDROIDID_KEY, UnityAdsDevice.getAndroidId(false));
@@ -146,7 +144,7 @@ public class UnityAdsWebView extends WebView {
 				initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_SCREENSIZE_KEY, UnityAdsDevice.getScreenSize());
 				initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_ZONES_KEY, UnityAdsWebData.getZoneManager().getZonesJson());
 
-				if(UnityAdsProperties.UNITY_VERSION != null && UnityAdsProperties.UNITY_VERSION.length() > 0) {
+				if (UnityAdsProperties.UNITY_VERSION != null && UnityAdsProperties.UNITY_VERSION.length() > 0) {
 				  initData.put(UnityAdsConstants.UNITY_ADS_WEBVIEW_DATAPARAM_UNITYVERSION_KEY, UnityAdsProperties.UNITY_VERSION);
 				}
 
@@ -180,7 +178,7 @@ public class UnityAdsWebView extends WebView {
 		_webAppLoaded = false;
 		setupUnityAdsView();
 		loadUrl(_url);
-		
+
 		if (Build.VERSION.SDK_INT > 8) {
 			setOnLongClickListener(new OnLongClickListener() {
 				@Override
@@ -191,64 +189,61 @@ public class UnityAdsWebView extends WebView {
 			setLongClickable(false);
 		}
 	}
-	
-	@SuppressWarnings("deprecation")
+
+	@SuppressWarnings({"deprecation"})
 	@SuppressLint("SetJavaScriptEnabled")
 	private void setupUnityAdsView ()  {
 		getSettings().setJavaScriptEnabled(true);
-		
-		if (_url != null && _url.indexOf("_raw.html") != -1) {
+
+		if (_url != null && _url.contains("_raw.html")) {
 			getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
 			UnityAdsDeviceLog.debug("LOAD_NO_CACHE");
 		}
 		else {
-			if (Build.VERSION.SDK_INT < 17 ) {				
+			if (Build.VERSION.SDK_INT < 17) {
 				getSettings().setCacheMode(WebSettings.LOAD_NORMAL);
 			}
 			else {
 				getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
 			}
 		}
-		
+
 		String appCachePath = null;
 		if (getContext() != null && getContext().getCacheDir() != null) 
 			appCachePath = getContext().getCacheDir().toString();
-		
+
 		getSettings().setSupportZoom(false);
 		getSettings().setBuiltInZoomControls(false);
 		getSettings().setLightTouchEnabled(false);
 		getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
 		getSettings().setSupportMultipleWindows(false);
 		getSettings().setAllowFileAccess(false);
-		
+
 		setHorizontalScrollBarEnabled(false);
 		setVerticalScrollBarEnabled(false);		
-
 		setClickable(true);
 		setFocusable(true);
 		setFocusableInTouchMode(true);
 		setInitialScale(0);
-
 		setBackgroundColor(Color.BLACK);
 		setBackgroundDrawable(null);
 		setBackgroundResource(0);
-		
 		setWebViewClient(new UnityAdsViewClient());
 		setWebChromeClient(new UnityAdsViewChromeClient());
 
 		if (appCachePath != null) {
 			boolean appCache = true;
-			
+
 			if (Build.VERSION.SDK_INT <= 7) {
 				appCache = false;
 			}
-  
+
 			getSettings().setAppCacheEnabled(appCache);
 			getSettings().setDomStorageEnabled(true);
 			getSettings().setAppCacheMaxSize(1024*1024*10);
 			getSettings().setAppCachePath(appCachePath);
 		}
-		
+
 		UnityAdsDeviceLog.debug("Adding javascript interface");
 		addJavascriptInterface(_webBridge, "unityadsnativebridge");
 	}
@@ -256,28 +251,30 @@ public class UnityAdsWebView extends WebView {
 	/* SUBCLASSES */
 
 	private class UnityAdsViewChromeClient extends WebChromeClient {
+		@SuppressWarnings("deprecation")
+		@Override
 		public void onConsoleMessage(String message, int lineNumber, String sourceID) {
 			String sourceFile = sourceID;
 			File tmp = null;
-			
+
 			try {
 				tmp = new File(sourceID);
 			}
 			catch (Exception e) {
 				UnityAdsDeviceLog.error("Could not handle sourceId: " + e.getMessage());
 			}
-			
-			if (tmp != null && tmp.getName() != null)
+
+			if (tmp != null)
 				sourceFile = tmp.getName();
-			
+
 			UnityAdsDeviceLog.debug("JavaScript (sourceId=" + sourceFile + ", line=" + lineNumber + "): " + message);
 		}
-		
-		public void onReachedMaxAppCacheSize(long spaceNeeded, long totalUsedQuota, WebStorage.QuotaUpdater quotaUpdater) {
+
+		public void onReachedMaxAppCacheSize(long spaceNeeded, long totalUsedQuota, @NonNull WebStorage.QuotaUpdater quotaUpdater) {
 			quotaUpdater.updateQuota(spaceNeeded * 2);
 		}
 	}
-	
+
 	private class UnityAdsViewClient extends WebViewClient {
 		@Override
 		public void onPageFinished (WebView webview, String url) {
@@ -288,13 +285,13 @@ public class UnityAdsWebView extends WebView {
 				_listener.onWebAppLoaded();
 			}
 		}
-		
+
 		@Override
 		public boolean shouldOverrideUrlLoading (WebView view, String url) {
 			UnityAdsDeviceLog.debug("Trying to load url: " + url);
 			return false;
 		}
-		
+
 		@Override
 		public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {		
 			UnityAdsDeviceLog.error(errorCode + " (" + failingUrl + ") " + description);
@@ -307,33 +304,31 @@ public class UnityAdsWebView extends WebView {
 		}	
 	}
 
-	
 	/* PRIVATE CLASSES */
-	
+
 	private class UnityAdsJavascriptRunner implements Runnable {
-		
 		private String _jsString = null;
 		private WebView _webView = null;
-		
+
 		public UnityAdsJavascriptRunner (String jsString, WebView webView) {
 			_jsString = jsString;
 			_webView = webView;
 		}
-		
+
 		@Override
 		public void run() {
 			if (_jsString != null) {
 				try {
 					if(Build.VERSION.SDK_INT >= 19) {
-						try
-						{
+						try {
 							Method evaluateJavascript = WebView.class.getMethod("evaluateJavascript", String.class, ValueCallback.class);
 							evaluateJavascript.invoke(_webView, _jsString, null);
 						}
 						catch (Exception e) {
 							UnityAdsDeviceLog.error("Could not invoke evaluateJavascript");
 						}
-					} else {
+					}
+					else {
 						loadUrl(_jsString);
 					}
 				}
