6d0acbc92ab2b3bd72d0147b970abbffc04aa7fa
Janne Nikula <jannen@unity3d.com>
2015-12-09 13:31:59 +0200

    Release 1.5.5

--- ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
+++ ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
@@ -12,7 +12,7 @@
 #import "../UnityAdsDevice/UnityAdsDevice.h"
 #import "UnityAdsCacheManager.h"
 
-NSString * const kUnityAdsVersion = @"1504";
+NSString * const kUnityAdsVersion = @"1505";
 
 @implementation UnityAdsProperties
 
